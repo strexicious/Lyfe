d235d72c71394f7c14b22ab7f784df45fcef3465
Strexicious <strexicious@gmail.com>
2018-02-25 18:56:26 +0100

    sfml enabled

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC=g++
 CFLAGS=
-LIBS=#-lsfml-graphics -lsfml-window -lsfml-system
+LIBS=-lsfml-graphics -lsfml-window -lsfml-system
 
 all:
 	$(CC) $(LIBS) -o lyfe main.cc
--- main.cc
+++ main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 #include "gol.hh"
 
 using namespace std;
@@ -9,11 +10,11 @@ constexpr unsigned HEIGHT = 10;
 int main() {
     const GoL<HEIGHT, WIDTH>::Grid pattern = {{
         {false, false, false, false, false, false, false, false, false, false},
-        {false, false, false, false, false, false, false, false, false, true},
-        {false, false, false, false, false, false, false, false, false, true},
-        {false, false, false, false, false, false, false, false, false, true},
         {false, false, false, false, false, false, false, false, false, false},
         {false, false, false, false, false, false, false, false, false, false},
+        {false, false, false, false, false, true , false, false, false, false},
+        {false, false, false, true , false, true , false, false, false, false},
+        {false, false, false, false, true , true , false, false, false, false},
         {false, false, false, false, false, false, false, false, false, false},
         {false, false, false, false, false, false, false, false, false, false},
         {false, false, false, false, false, false, false, false, false, false},
@@ -21,6 +22,40 @@ int main() {
     }};
 
     GoL<HEIGHT, WIDTH> gol(pattern);
+    GoL<HEIGHT, WIDTH>::Grid gol_state = gol.state();
+
+    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "SFML window");
+    window.setFramerateLimit(10);
+    sf::Image gol_draw;
+    gol_draw.create(WIDTH, HEIGHT);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        for (unsigned i = 0; i < HEIGHT; i++) {
+            for (unsigned j = 0; j < WIDTH; ++j) {
+                if (gol_state[i][j])
+                    gol_draw.setPixel(j, i, sf::Color(0xffffffff));
+                else
+                    gol_draw.setPixel(j, i, sf::Color(0x000000ff));
+            }
+        }
+
+        sf::Texture gol_text;
+        gol_text.loadFromImage(gol_draw);
+        sf::Sprite sprite(gol_text);
+
+        window.clear();
+        window.draw(sprite);
+        window.display();
+        gol_state = gol.update();
+    }
+
 
     return 0;
 }
