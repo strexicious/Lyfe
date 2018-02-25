15caafd690069bd85090347d3fc4c753c3223100
Strexicious <strexicious@gmail.com>
2018-02-25 20:18:46 +0100

    I think it's good for now

--- main.cc
+++ main.cc
@@ -1,38 +1,40 @@
 #include <iostream>
+#include <random>
 #include <SFML/Graphics.hpp>
 #include "gol.hh"
 
 using namespace std;
 
-constexpr unsigned WIDTH = 10;
-constexpr unsigned HEIGHT = 10;
+constexpr unsigned WIDTH = 500;
+constexpr unsigned HEIGHT = 500;
 
 int main() {
-    const GoL<HEIGHT, WIDTH>::Grid pattern = {{
-        {false, false, false, false, false, false, false, false, false, false},
-        {false, false, false, false, false, false, false, false, false, false},
-        {false, false, false, false, false, false, false, false, false, false},
-        {false, false, false, false, false, true , false, false, false, false},
-        {false, false, false, true , false, true , false, false, false, false},
-        {false, false, false, false, true , true , false, false, false, false},
-        {false, false, false, false, false, false, false, false, false, false},
-        {false, false, false, false, false, false, false, false, false, false},
-        {false, false, false, false, false, false, false, false, false, false},
-        {false, false, false, false, false, false, false, false, false, false},
-    }};
 
-    GoL<HEIGHT, WIDTH> gol(pattern);
+    GoL<HEIGHT, WIDTH>::Grid random_state;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::bernoulli_distribution dist;
+    for (unsigned i = 0; i < random_state.size(); ++i) {
+        for (unsigned j = 0; j < random_state[0].size(); ++j) {
+            random_state[i][j] = (bool) dist(gen);
+        }
+    }
+
+    GoL<HEIGHT, WIDTH> gol(random_state);
     GoL<HEIGHT, WIDTH>::Grid gol_state = gol.state();
 
-    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "SFML window");
-    window.setFramerateLimit(10);
+    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Lyfe");
+    window.setFramerateLimit(30);
     sf::Image gol_draw;
     gol_draw.create(WIDTH, HEIGHT);
+    sf::Texture gol_text;
+    gol_text.loadFromImage(gol_draw);
+    sf::Sprite gol_spr(gol_text);
 
     while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
@@ -46,16 +48,13 @@ int main() {
             }
         }
 
-        sf::Texture gol_text;
-        gol_text.loadFromImage(gol_draw);
-        sf::Sprite sprite(gol_text);
+        gol_text.update(gol_draw);
 
         window.clear();
-        window.draw(sprite);
+        window.draw(gol_spr);
         window.display();
         gol_state = gol.update();
     }
-
 
     return 0;
 }
