3a8f6d381693f4bd208b065f4eb9a5fa1876d1cb
Strexicious <strexicious@gmail.com>
2018-02-25 18:13:25 +0100

    finished the basic GoL implementation

--- gol.hh
+++ gol.hh
@@ -6,7 +6,7 @@ template<unsigned N, unsigned M>
 class GoL {
 
 public:
-    // Common unsignederface type between this class and it's user
+    // Common interface type between this class and it's user
     using Grid = Array2D<bool, N, M>;
 
 private:
@@ -30,15 +30,35 @@ public:
 
 template<unsigned N, unsigned M>
 unsigned GoL<N, M>::get_neighbours_count(unsigned row, unsigned col) const {
-    // TODO
-    return 0;
+    unsigned negs = 0;
+    if (buffer_cur[((row-1 % N) + N) % N][((col-1 % M) + M) % M])
+        ++negs;
+    if (buffer_cur[((row-1 % N) + N) % N][col])
+        ++negs;
+    if (buffer_cur[((row-1 % N) + N) % N][(col+1) % M])
+        ++negs;
+    if (buffer_cur[row][((col-1 % M) + M) % M])
+        ++negs;
+    if (buffer_cur[row][(col+1) % M])
+        ++negs;
+    if (buffer_cur[(row+1) % N][((col-1 % M) + M) % M])
+        ++negs;
+    if (buffer_cur[(row+1) % N][col])
+        ++negs;
+    if (buffer_cur[(row+1) % N][(col+1) % M])
+        ++negs;
+    
+    return negs;
 }
 
 template<unsigned N, unsigned M>
 class GoL<N, M>::Grid GoL<N, M>::update() {
     buffer_res = buffer_cur;
+    
+    // for every cell...
     for (unsigned i = 0; i < N; ++i) {
         for (unsigned j = 0; j < M; ++j) {
+            // ...apply the rules
             unsigned negs = get_neighbours_count(i, j);
             if (buffer_cur[i][j] && (negs < 2 || negs > 3))
                 buffer_res[i][j] = false;
--- main.cc
+++ main.cc
@@ -9,10 +9,10 @@ constexpr unsigned HEIGHT = 10;
 int main() {
     const GoL<HEIGHT, WIDTH>::Grid pattern = {{
         {false, false, false, false, false, false, false, false, false, false},
+        {false, false, false, false, false, false, false, false, false, true},
+        {false, false, false, false, false, false, false, false, false, true},
+        {false, false, false, false, false, false, false, false, false, true},
         {false, false, false, false, false, false, false, false, false, false},
-        {false, false, false, true, false, false, false, false, false, false},
-        {false, false, false, true, false, false, false, false, false, false},
-        {false, false, false, true, false, false, false, false, false, false},
         {false, false, false, false, false, false, false, false, false, false},
         {false, false, false, false, false, false, false, false, false, false},
         {false, false, false, false, false, false, false, false, false, false},
@@ -21,21 +21,6 @@ int main() {
     }};
 
     GoL<HEIGHT, WIDTH> gol(pattern);
-
-    for (auto &row : gol.state()) {
-        for (auto cell : row)
-            cout << (cell ? 'O' : '-') << " ";
-        cout << '\n';
-    }
-
-    cout << "=========\n";
-
-    auto updated = gol.update();
-    for (auto &row : updated) {
-        for (auto cell : row)
-            cout << (cell ? 'O' : '-') << " ";
-        cout << '\n';
-    }
 
     return 0;
 }
