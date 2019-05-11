7595b55d34614bc3dedc701c2f0f6f5e3d48f1f0
Strexicious <strexicious@gmail.com>
2019-05-11 14:14:19 +0200

    cpp got mixed in with rust

--- Array2D.hh
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <array>
-
-template<class T, int N, int M>
-using Array2D = std::array<std::array<T, M>, N>;
--- Makefile
+++
@@ -1,6 +0,0 @@
-CC=g++
-CFLAGS=
-LIBS=-lsfml-graphics -lsfml-window -lsfml-system
-
-all:
-	$(CC) $(LIBS) -o lyfe main.cc
